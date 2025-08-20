04ec7be44c7a4c98ac42b376f74692b397ef9f2e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-20 12:26:34 -0300

    Add initial implementation of shape and messaging classes for challenges 1 and 2, and create project file for challenge 3

---
+++ c#/challenges/challange3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+public abstract class Pet
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+  public string OwnerName { get; set; }
+}
+
+public abstract class Dog : Pet
+{
+  public string Bread { get; set; }
+  public Bark()
+  {
+    return Console.ReadLine("Au AU aU Au");
+  }
+
+}
