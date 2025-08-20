14d4cc4cf44a722869a5b496b6e6bd58d8b1d7a9
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-20 12:40:24 -0300

    Refactor Program class to improve option selection flow and enhance dog addition logic

--- c#/challenges/challange3/Program.cs
+++ c#/challenges/challange3/Program.cs
@@ -36,18 +36,34 @@ class Program
     while (!exit)
     {
       Console.Clear();
-      Console.WriteLine("Choose an option:");
       Console.WriteLine("1 - Add new Dog");
       Console.WriteLine("2 - Add new Cat");
       Console.WriteLine("3 - List all pets");
       Console.WriteLine("4 - Make a pet sound (choose pet and call Bark/Meow)");
 
+      Console.WriteLine("Choose an option:");
       string option = Console.ReadLine();
 
       switch (option)
       {
+        case "1":
+          Dog d = new();
+          Console.WriteLine("Enter dog's name:");
+          d.Name = Console.ReadLine();
+
+          Console.WriteLine("Enter dog's age:");
+          d.Age = int.Parse(Console.ReadLine());
+
+          Console.WriteLine("Enter dog's owner name:");
+          d.OwnerName = Console.ReadLine();
+
+          Console.WriteLine("Enter dog's breed:");
+          d.Breed = Console.ReadLine();
+
+          pets.Add(d);
+          break;
               
-            }
+      }
     }
   }
   
