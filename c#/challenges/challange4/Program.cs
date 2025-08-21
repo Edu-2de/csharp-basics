a646f94662643ce0afe1e0a7e9fc491e57b84c89
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-08-21 19:27:13 -0300

    Add Book class to extend MediaItem with page count and summary display

--- c#/challenges/challange4/Program.cs
+++ c#/challenges/challange4/Program.cs
@@ -3,4 +3,14 @@
   public string? Title { get; set; }
   public int Year { get; set; }
   public string? AuthorOrDirector { get; set; }
-} 
+}
+
+public class Book : MediaItem
+{
+  public int NumberOfPages { get; set; }
+
+  public static void ShowSummary()
+  {
+    Console.WriteLine($"Title: {Title}, Number of pages: {NumberOfPages}");
+  }
+}
