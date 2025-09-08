b4f98bbcb3fd1781ba277db1d0763fec480fc873
Eduardo Da Silva Paim <85540781015@senacrs.edu.br>
2025-09-08 20:57:55 -0300

    Add properties to Pessoa and create Leitor class

--- c#/challenges/challange5/Program1.cs
+++ c#/challenges/challange5/Program1.cs
@@ -1,5 +1,14 @@
 namespace Biblioteca {
     public abstract class Pessoa {
-        
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        public string Endereco {get;set;}
+        public string Telefone {get;set;}
+        public string Email{get;set;}
+    }
+
+
+    public class Leitor:Pessoa{
+        public DateTime DataNascimento{get;set;}
     }
 }
