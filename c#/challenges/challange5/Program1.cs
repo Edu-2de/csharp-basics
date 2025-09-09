b7e1c14e3fb40a25bcc642c148a03379349e58a0
Eduardo Da Silva Paim <85540781015@senacrs.edu.br>
2025-09-08 21:08:27 -0300

    Add properties Genero and Disponivel to Program1 class

--- c#/challenges/challange5/Program1.cs
+++ c#/challenges/challange5/Program1.cs
@@ -44,7 +44,7 @@ namespace Biblioteca {
         public string Titulo {get; set;}
         public string Autor {get;set;}
         public int AnoPublicacao{get;set;}
-
-
+        public string Genero{get;set;}
+        public bool Disponivel  {get; set;}
     }
 }
